3954eea393262ae05f790905df457fe3387be36f
Wenying <iris290337415@gmail.com>
2020-02-22 18:58:40 -0500

    Updated data generation
    Added samplf_f2 to generate data using RFF.

--- code/exp/toy.py
+++ code/exp/toy.py
@@ -4,6 +4,12 @@ from __future__ import print_function
 
 import numpy as np
 
+from tensorflow.python.framework import test_util
+from tensorflow.python.keras import backend as keras_backend
+import exp.kernelized as kernel_layers
+import exp.kernelized_utils as kernelized_utils
+
+
 __all__ = [
     'x3_toy',
     'x3_gap_toy',
@@ -22,14 +28,14 @@ class toy_dataset(object):
         raise NotImplementedError
 
 
-class rbf_toy(toy_dataset):
+class rbf_toy2(toy_dataset):
     def __init__(self, variance=1.0, lengthscale=1.0, name='rbf'):
         self.variance = variance
         self.lengthscale = lengthscale
         self.x_min = 0
         self.x_max = 1
         self.y_std = np.sqrt(.001)
-        super(rbf_toy, self).__init__(name)
+        super(rbf_toy2, self).__init__(name)
 
     def sample_f(self, n_train_max, n_test, seed=0):
         self.X_train_max = np.random.uniform(self.x_min, self.x_max, (n_train_max, 1))
@@ -40,16 +46,32 @@ class rbf_toy(toy_dataset):
         self.f_train_max = f[:n_train_max].reshape(-1,1)
         self.f_test = f[-n_test:].reshape(-1,1)
 
+    def sample_f2(self, n_train_max, n_test, seed=0):
+        tst = test_util.TensorFlowTestCase()
+        rff_layer = kernel_layers.RandomFourierFeatures(
+            output_dim=2000,
+            kernel_initializer='gaussian',
+            trainable=True)
+        output_x = np.sqrt(2.0 / 2000) * rff_layer(self.X)
+        approx_K = kernelized_utils.inner_product(output_x, output_x)
+        with tst.cached_session() as sess:
+            keras_backend._initialize_variables(sess)
+            approx_K_eval = sess.run(approx_K)
+        f2 = np.random.multivariate_normal(np.zeros(self.X.shape[0]), approx_K_eval)
+        self.f2_train_max = f2[:n_train_max].reshape(-1, 1)
+        self.f2_test = f2[-n_test:].reshape(-1, 1)
 
     def train_samples(self, n_data=20, seed=0):
         np.random.seed(seed)
         idx_sample = np.random.choice(self.X_train_max.shape[0], n_data)
         epsilon = np.random.normal(0, self.y_std, (n_data, 1))
         y_train = self.f_train_max[idx_sample] + epsilon
-        return self.X_train_max[idx_sample], y_train
+        y_train2 = self.f2_train_max[idx_sample] + epsilon
+        return self.X_train_max[idx_sample], y_train, y_train2
 
     def test_samples(self, n_data=None, seed=0):
         np.random.seed(seed)
         epsilon = np.random.normal(0, self.y_std, (self.f_test.shape[0], 1))
         y_test = self.f_test + epsilon
-        return self.X_test, y_test
+        y_test2 = self.f2_test + epsilon
+        return self.X_test, y_test, y_test2
