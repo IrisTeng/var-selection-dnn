5f7cc15fffd9b68736e640e7475f30da2df51aa4
Jeremiah Zhe Liu <zhel90@gmail.com>
2020-04-17 18:42:48 -0700

    convert data type to float64

--- code/exp/util.py
+++ code/exp/util.py
@@ -2,20 +2,20 @@ import numpy as np
 import tensorflow as tf
 from scipy.stats import multivariate_normal
 
+
 def standardize_data(original_x_train, original_y_train, original_x_test, original_y_test,
-                     mean_x = None, std_x = None, mean_y = None, std_y = None):
-    
+                     mean_x=None, std_x=None, mean_y=None, std_y=None):
     if mean_x is not None and std_x is not None:
         mean_x, std_x = np.mean(original_x_train), np.std(original_x_train)
 
     if mean_y is not None and std_y is not None:
         mean_y, std_y = np.mean(original_y_train), np.std(original_y_train)
-    
-    train_x = (original_x_train - mean_x) / std_x
-    train_y = ((original_y_train - mean_y) / std_y).reshape(-1,1)
 
-    test_x = ((original_x_test - mean_x) / std_x).reshape(-1,1)
-    test_y = ((original_y_test - mean_y) / std_y).reshape(-1,1)
+    train_x = (original_x_train - mean_x) / std_x
+    train_y = ((original_y_train - mean_y) / std_y).reshape(-1, 1)
+
+    test_x = ((original_x_test - mean_x) / std_x).reshape(-1, 1)
+    test_y = ((original_y_test - mean_y) / std_y).reshape(-1, 1)
 
     return train_x, train_y, test_x, test_y
 
@@ -24,15 +24,18 @@ def standardize_data(original_x_train, original_y_train, original_x_test, origin
 
 def rmse(f, f_pred):
     # predictive root mean squared error (RMSE)
-    return np.sqrt(np.mean((f - f_pred)**2))
+    return np.sqrt(np.mean((f - f_pred) ** 2))
+
 
 def picp(f, f_pred_lb, f_pred_ub):
     # prediction interval coverage (PICP)
     return np.mean(np.logical_and(f >= f_pred_lb, f <= f_pred_ub))
 
+
 def mpiw(f_pred_lb, f_pred_ub):
     # mean prediction interval width (MPIW)
     return np.mean(f_pred_ub - f_pred_lb)
+
 
 def test_log_likelihood(mean, cov, test_y):
     return multivariate_normal.logpdf(test_y.reshape(-1), mean.reshape(-1), cov)
@@ -54,7 +57,7 @@ def minibatch_woodbury_update(X, H_inv):
     """
     batch_size = tf.shape(X)[0]
 
-    M0 = tf.eye(batch_size) + tf.matmul(X, tf.matmul(H_inv, X, transpose_b=True))
+    M0 = tf.eye(batch_size, dtype=tf.float64) + tf.matmul(X, tf.matmul(H_inv, X, transpose_b=True))
     M = tf.matrix_inverse(M0)
     B = tf.matmul(X, H_inv)
     H_new = H_inv - tf.matmul(B, tf.matmul(M, B), transpose_a=True)
@@ -72,5 +75,3 @@ def compute_inverse(X, sig_sq=1):
 
 def split_into_batches(X, batch_size):
     return [X[i:i + batch_size] for i in range(0, len(X), batch_size)]
-
-
